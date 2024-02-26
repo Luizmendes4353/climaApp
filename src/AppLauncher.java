54d592a7129a61a326f3db6e1463e2c7c757c379
luizfelipe <luizmendes5849@gmail.com>
2024-02-26 01:28:46 -0300

    Completed

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -2,13 +2,11 @@ import javax.swing.*;
 
 public class AppLauncher {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable(){
-           @Override
-            public void run(){
-               new WeatherAppGui(). setVisible(true);
-
-               System.out.println(WeatherApp.getLocationData("Tokyo"));
-           }
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new WeatherAppGui().setVisible(true);
+            }
         });
     }
 }
